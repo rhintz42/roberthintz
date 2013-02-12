710275c4b53f8b6b997fae86b75a7eb22194aad8
Robert Hintz <rhintz42@stanford.edu>
2013-02-11 18:24:55 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Roberthintz::Application.config.secret_token = 'bc6ef6a40bd78d9b685574934859c18ac3c5e9c4cece8b58df92aab1a1d3cd686682acf99d5d5952d425f3fbd6c9479603a2badccc1d578fc0b65d876348d6cb'
