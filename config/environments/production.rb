f05809c9c020aa99d96e2f61c23cb7e4a99f7880
Robert Hintz <rhintz42@stanford.edu>
2013-02-27 03:10:14 -0800

    Try to add configuration for IMGKit for Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,10 @@ Roberthintz::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  
+  IMGKit.configure do |config|
+    config.wkhtmltoimage = Rails.root.join('bin', 'wkhtmltoimage-amd64').to_s if ENV['RACK_ENV'] == 'production'
+  end
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
