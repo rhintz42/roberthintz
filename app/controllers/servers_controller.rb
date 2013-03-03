ea2bda78cab09ef4467782ea5b9f958b0424b59d
Robert Hintz <rhintz42@stanford.edu>
2013-03-03 03:53:19 -0800

    Added servers js ajax files

--- app/controllers/servers_controller.rb
+++ app/controllers/servers_controller.rb
@@ -5,6 +5,7 @@ class ServersController < ApplicationController
   	respond_to do |format|
   		format.html
   		format.json { render json: @servers }
+      format.js
   	end
   end
 
@@ -14,6 +15,7 @@ class ServersController < ApplicationController
   	respond_to do |format|
   		format.html
   		format.json { render json: @server }
+      format.js
   	end
   end
 
@@ -23,11 +25,17 @@ class ServersController < ApplicationController
   	respond_to do |format|
   		format.html
   		format.json { render json: @server }
+      format.js
   	end
   end
 
   def edit
   	@server = Server.find(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   def create
@@ -51,9 +59,11 @@ class ServersController < ApplicationController
   		if @server.update_attributes(params[:server])
   			format.html { redirect_to @server, notice: 'User was successfully updated!'}
   			format.json { head :no_content }
+        format.js { redirect_to @server, notice: 'User was successfully updated!' }
   		else
   			format.html { render action: "edit" }
   			format.json { render json: @server.errors, status: :unprocessed_entity }
+        format.js { render action: "edit" }
   		end
   	end
   end
